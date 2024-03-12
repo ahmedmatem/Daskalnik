68d4b77bb01255991eb7b6eb88315d7dfed45c80
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-03-12 21:11:51 +0200

    Move migrations in Migrations folder

--- Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,18 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Infrastructure.Data.Migration
+namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240312190528_InitialMigration")]
-    partial class InitialMigration
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
