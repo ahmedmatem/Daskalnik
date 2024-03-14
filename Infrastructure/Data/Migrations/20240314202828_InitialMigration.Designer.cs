8f1fef44cb1cf49e49efbee3000efac89ca4abb3
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-03-14 22:31:27 +0200

    Migrations folder moved

--- Infrastructure/Data/Migrations/20240314202828_InitialMigration.Designer.cs
+++ Infrastructure/Data/Migrations/20240314202828_InitialMigration.Designer.cs
@@ -4,16 +4,18 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240314202828_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
