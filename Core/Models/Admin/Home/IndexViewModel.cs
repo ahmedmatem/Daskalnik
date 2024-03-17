b358fd1cbd48aa8a06cbb6d5874b7051bddc3a6e
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-03-17 23:08:05 +0200

    Service layer added

---
+++ Core/Models/Admin/Home/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models.Admin.Home
+{
+    public class IndexViewModel
+    {
+        public int SchoolsCount { get; set; }
+
+        public int TeachersCount { get; set; }
+
+        public int GroupsCount { get; set; }
+
+        public int StudentsCount { get; set; }
+    }
+}
---
+++ Core/Services/SchoolService.cs
@@ -0,0 +1,24 @@
+﻿using Core.Contracts;
+using Infrastructure.Data.DataRepository;
+using Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Services
+{
+    public class SchoolService : ISchoolService
+    {
+        private readonly IRepository repository;
+
+        public SchoolService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<int> GetSchoolsCountAsync()
+        {
+            return await repository.All<School>()
+                .Where(s => !s.IsDeleted)
+                .CountAsync();
+        }
+    }
+}
