4494c74f2a042df23a50236e2441c6336700a78d
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-03-17 23:07:45 +0200

    Unit of Work added

---
+++ Infrastructure/Data/DataRepository/Repository.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DataRepository
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class 
+            => context.Set<T>();
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+    }
+}
