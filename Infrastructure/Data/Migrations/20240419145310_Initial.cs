0b7138885c9022899ad5c25e141740c2c545fc1e
Ahmed Matem <ahmedmatem@gmail.com>
2024-09-29 19:34:09 +0300

    Reset db migrations

--- Infrastructure/Data/Migrations/20240419145310_Initial.cs
+++ Infrastructure/Data/Migrations/20240419145310_Initial.cs
@@ -186,7 +186,6 @@ namespace Infrastructure.Data.Migrations
                     IconUrl = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "Group icon url."),
                     SchoolId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "School identifier in which the group belongs to. "),
                     TeacherId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Group creater identifier."),
-                    StudentId = table.Column<string>(type: "nvarchar(450)", nullable: true),
                     IsDeleted = table.Column<bool>(type: "bit", nullable: false, comment: "Indicate a record in table as deleted or not."),
                     DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true, comment: "Mark the date of deleting the record in the table."),
                     CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true, comment: "Mark the date of created the record on in the table."),
@@ -201,11 +200,6 @@ namespace Infrastructure.Data.Migrations
                         principalTable: "Schools",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Groups_Students_StudentId",
-                        column: x => x.StudentId,
-                        principalTable: "Students",
-                        principalColumn: "Id");
                     table.ForeignKey(
                         name: "FK_Groups_Teachers_TeacherId",
                         column: x => x.TeacherId,
@@ -511,16 +505,6 @@ namespace Infrastructure.Data.Migrations
                 column: "NormalizedUserName",
                 unique: true,
                 filter: "[NormalizedUserName] IS NOT NULL");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Groups_SchoolId",
-                table: "Groups",
-                column: "SchoolId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Groups_StudentId",
-                table: "Groups",
-                column: "StudentId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Groups_TeacherId",
