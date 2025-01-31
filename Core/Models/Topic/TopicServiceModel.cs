3560ef84d6f987aa4ff99ffe16cea3cc99025288
Ahmed Matem <ahmedmatem@gmail.com>
2025-01-31 18:55:18 +0200

    Create TopicServiceModel

---
+++ Core/Models/Topic/TopicServiceModel.cs
@@ -0,0 +1,57 @@
+﻿namespace Core.Models.Topic
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Infrastructure.Constants.DataConstants;
+    using static Infrastructure.Data.ErrorMessages;
+
+    using Core.Models.Resource;
+
+    public class TopicServiceModel
+    {
+        /// <summary>
+        /// Topic unique identifier.
+        /// </summary>
+        public string TopicId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Topic name.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(
+            TopicNameMaxLength, 
+            MinimumLength = TopicNameMinLength,
+            ErrorMessage = StringLengthErrorMessage)]
+        [Display(Name = "Име")]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Topic description.
+        /// </summary>
+        [MaxLength(TopicDescriptionMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        [Display(Name = "Описание")]
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Topic contents items separated each in a new line.
+        /// </summary>
+        [Display(Name = "Съдържание")]
+        public string Contents { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Unique group identifier
+        /// </summary>
+        public string GroupId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Unique identifier of the creator of the group and topics in the group.
+        /// </summary>
+        public string CreatorId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Collection of resources for the topic in the group.
+        /// </summary>
+        public IEnumerable<ResourceServiceModel> Resources { get; set; }
+            = new HashSet<ResourceServiceModel>();
+    }
+}
