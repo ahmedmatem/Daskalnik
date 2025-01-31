470e91d075b529c5ece1bd8dbbbc99dd4e0fed14
Ahmed Matem <ahmedmatem@gmail.com>
2025-01-31 19:05:06 +0200

    Reverse map topic and TopicServiceModel

--- Web/Automapper/MappingProfile.cs
+++ Web/Automapper/MappingProfile.cs
@@ -1,12 +1,14 @@
-﻿using AutoMapper;
-
-namespace Web.Automapper
+﻿namespace Web.Automapper
 {
+    using AutoMapper;
+    using Core.Models.Topic;
+    using Infrastructure.Data.Models;
+
     public class MappingProfile : Profile
     {
         public MappingProfile() 
         {
-            
+            CreateMap<TopicServiceModel, Topic>().ReverseMap();
         }
     }
 }
